8af386e770a8ba506cf098cb0ef814329c0b0a71
Brian, Song <blackdogcode@gmail.com>
2020-01-16 20:52:00 +0900

    Hello Maven

---
+++ hello-maven/src/main/java/academy/learningprogramming/hello_maven/App.java
@@ -0,0 +1,13 @@
+package academy.learningprogramming.hello_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
